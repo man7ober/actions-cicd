075a124e1c2313dcda1a0bd5278ee8a8a63ad71a
man7ober <man7ober@gmail.com>
2026-07-16 09:35:49 +0530

    restructure and fix code

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,6 +2,6 @@ output "security_group_id" {
   value = aws_security_group.my_sg.id
 }
 
-output "ec2_instance_id" {
-  value = aws_instance.my_instance.id
+output "ec2_public_ip" {
+  value = aws_instance.my_instance.public_ip
 }
